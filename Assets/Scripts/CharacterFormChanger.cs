53c205c43a053f13082a9f4e2d78c769b8e6c424
OdericoOfficial <2919128969@qq.com>
2022-11-18 16:10:35 +0800

    CharacterFormChanger完成

--- Assets/Scripts/CharacterFormChanger.cs
+++ Assets/Scripts/CharacterFormChanger.cs
@@ -1,12 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Events;
+using static CharacterModel;
 
-public class CharacterFormChanger : MonoBehaviour
+public class CharacterFormChanger : ObservableMonoBehavior
 {
     CharacterController _chara;
+
+    /// <summary>
+    /// 比较器
+    /// </summary>
+    public class CharacterFormEqualityComparer : IEqualityComparer<CharacterForm>
+    {
+        public bool Equals(CharacterForm x, CharacterForm y)
+        {
+            if (x == y)
+                return true;
+
+            switch (y)
+            {
+                case CharacterForm.Ice:
+                    return CharacterController.Instance.Temperature != Temperature.Zero;
+                case CharacterForm.Mist:
+                    return CharacterController.Instance.Temperature != Temperature.Boil;
+                case CharacterForm.Water:
+                    return CharacterController.Instance.Temperature != Temperature.Standard;
+                default:
+                    return false;
+            }
+        }
+
+        public int GetHashCode(CharacterForm obj)
+            => obj.GetHashCode();
+    }
+
+    private CharacterFormEqualityComparer comparer;
+
+    /// <summary>
+    /// 伤害
+    /// </summary>
+    public long Damage;
+    
+    /// <summary>
+    /// 若要改变形态，直接对这个属性赋值即可
+    /// </summary>
+    public CharacterForm Form
+    {
+        get => _chara.Form;
+        set => SetProperty(ref _chara.Form, value, comparer);
+    }
+
+    /// <summary>
+    /// 由提供外部的动画改变委托，参数为本次形态改变的结果
+    /// </summary>
+    public UnityAction<CharacterForm> OnAnimationChanging { get; set; }
+
+    /// <summary>
+    /// 由提供外部的贴图改变委托，参数为本次形态改变的结果
+    /// </summary>
+    public UnityAction<CharacterForm> OnSpriteChanging { get; set; }
+
+    /// <summary>
+    /// 由提供外部的HP归零委托，参数为本次形态变换导致的最后一次伤害
+    /// </summary>
+    public UnityAction<long> OnHPEmpty { get; set; }
+
     private void Awake()
     {
         _chara = GetComponent<CharacterController>();
+        comparer = new CharacterFormEqualityComparer();
+    }
+
+    private void OnCharacterFormChanged(object sender, PropertyChangedEventArgs args)
+    {
+        if (args.PropertyName == nameof(Form))
+        {
+            OnAnimationChanging(Form);
+            OnSpriteChanging(Form);
+
+            if (_chara.HP - Damage <= 0)
+                OnHPEmpty(Damage);
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        Dispose();
     }
 }
---
+++ Assets/Scripts/ObservableMonoBehavior.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+#nullable enable
+public abstract class ObservableMonoBehavior : MonoBehaviour, INotifyPropertyChanged, INotifyPropertyChanging, IDisposable
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    public virtual void Dispose()
+    {
+        PropertyChanged = null;
+        PropertyChanging = null;
+    }
+
+    protected bool SetProperty<TValue>(ref TValue oldValue, TValue newValue,
+        [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<TValue>.Default.Equals(oldValue, newValue))
+            return false;
+
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        oldValue = newValue;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        return true;
+    }
+
+    protected bool SetProperty<TValue>(ref TValue oldValue, TValue newValue,
+        IEqualityComparer<TValue> comparer, [CallerMemberName] string? propertyName = null)
+    {
+        if (comparer == null)
+            throw new ArgumentNullException("Comparer cannot be null.");
+
+        if (comparer.Equals(oldValue, newValue))
+            return false;
+
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        oldValue = newValue;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        return true;
+    }
+
+    protected bool SetProperty<TModel, TValue>(TValue oldValue, TValue newValue,
+        TModel model, Action<TModel, TValue> callBack, [CallerMemberName] string? propertyName = null)
+    {
+        if (model == null)
+            throw new ArgumentNullException("Model cannot be null.");
+
+        if (EqualityComparer<TValue>.Default.Equals(oldValue, newValue))
+            return false;
+
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        callBack.Invoke(model, newValue);
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        return true;
+    }
+
+    protected bool SetProperty<TModel, TValue>(TValue oldValue, TValue newValue, TModel model,
+        IEqualityComparer<TValue> comparer, Action<TModel, TValue> callBack, [CallerMemberName] string? propertyName = null)
+    {
+        if (model == null)
+            throw new ArgumentNullException("Model cannot be null.");
+
+        if (comparer == null)
+            throw new ArgumentNullException("Comparer cannot be null.");
+
+        if (EqualityComparer<TValue>.Default.Equals(oldValue, newValue))
+            return false;
+
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        callBack.Invoke(model, newValue);
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        return true;
+    }
+}
