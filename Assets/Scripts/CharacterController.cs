5fbd765e09ad5d215046bc6d7664a1fc2e64fbfb
buger404 <ris_vb@126.com>
2022-11-18 14:13:21 +0800

    修复架构

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -13,18 +13,23 @@ public class CharacterController : MonoBehaviour
     public bool isHandstand = false;
     public long HP = 100;
 
+    [HideInInspector]
     public SpriteRenderer spriteRenderer;
+    [HideInInspector]
     public CharacterAnimator characterAnimator;
+    [HideInInspector]
     public CharacterFormChanger characterFormChanger;
+    [HideInInspector]
     public MoveController moveController;
+    [HideInInspector]
     public HPController hpController;
 
     private void Awake()
     {
-        characterAnimator = new CharacterAnimator(this);
-        characterFormChanger = new CharacterFormChanger(this);
-        moveController = new MoveController(this);
-        hpController = new HPController(this);
+        characterAnimator = gameObject.AddComponent<CharacterAnimator>();
+        characterFormChanger = gameObject.AddComponent<CharacterFormChanger>();
+        moveController = gameObject.AddComponent<MoveController>();
+        hpController = gameObject.AddComponent<HPController>();
 
         spriteRenderer = GetComponent<SpriteRenderer>();
 
