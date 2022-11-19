99ae99a7fa39a02393d17a250b23f6b9477af6dd
OdericoOfficial <2919128969@qq.com>
2022-11-19 14:51:35 +0800

    暂停+改了下bug
    看来unity是反射运行OnDestroy等等的生命周期函数，搞得我要加NullReference判断

--- Assets/Scripts/Interact/SavePoint/SavePoint.cs
+++ Assets/Scripts/Interact/SavePoint/SavePoint.cs
@@ -12,45 +12,19 @@ public class SavePoint : InteractBase
     /// <summary>
     /// 逻辑代码全在ViewModel里
     /// </summary>
-    private SavePointViewModel _savePoint { get; set; }
+    private SavePointViewModel _savePoint 
+        => ServiceLocator.Instance?.SavePoint;
 
     /// <summary>
     /// 触碰到检查点后可能会有一些动画效果等等，留一个回调
     /// </summary>
     public static event UnityAction<SavePoint> OnSavePointTriggered;
-    
-    /// <summary>
-    /// 由外部提供移动暂停方法
-    /// </summary>
-    public UnityAction MovePause { get; set; }
-
-    /// <summary>
-    /// 由外部提供移动恢复方法
-    /// </summary>
-    public UnityAction MoveRestart { get; set; }
-
-    /// <summary>
-    /// 是否为出生点（关卡初始位置，还未触发任何检查点时）
-    /// </summary>
-    public bool isBornPoint = false;
-
-    private void Start()
-    {
-        _savePoint = ServiceLocator.Instance.SavePoint;
-        _savePoint.MovePause = MovePause;
-        _savePoint.MoveRestart = MoveRestart;
-        if (isBornPoint)
-        {
-            Interact();
-        }
-    }
 
     public override bool Interact()
     {
         _savePoint.RecentSavePosition = CharacterController.Instance.GetComponent<Transform>().position;
-        _savePoint.RecentHP = 40; // 固定恢复到40，忘了说明了qwq CharacterController.Instance.HP;
+        _savePoint.RecentHP = CharacterController.Instance.HP;
         OnSavePointTriggered?.Invoke(this);
         return true;
     }
-
 }
---
+++ Assets/Scripts/PausePanel/PausePanel.cs
@@ -0,0 +1,21 @@
+﻿using GenericToolKit.Mvvm;
+using GenericToolKit.Mvvm.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PausePanel : ObservablePanelMono<PausePanelViewModel>
+{
+    public override PausePanelViewModel Panel
+        => ServiceLocator.Instance?.PausePanel;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Panel.SetActive();
+    }
+}
