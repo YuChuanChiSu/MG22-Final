e897798b085c2e428c3eec091f07f87312699aea
buger404 <ris_vb@126.com>
2022-11-19 17:17:08 +0800

    新增音效、音乐播放器，场景切换器

---
+++ Assets/Scripts/Audio/BGMController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BGMController : MonoBehaviour
+{
+    public static BGMController Active = null;
+    public static bool PausedDueToBGM = false;
+    public AudioClip bgm;
+    private AudioSource source;
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = bgm;
+        if (Active != null)
+        {
+            if (Active.source.clip != bgm)
+            {
+                Loading.activeAnimator.SetFloat("speed", 0);
+                PausedDueToBGM = true;
+                Active.GetComponent<Animator>().Play("BGMVolumeFadeOut", 0, 0.0f);
+                source.Stop();
+                Active = this;
+                DontDestroyOnLoad(this.gameObject);
+                return;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+        Active = this;
+        DontDestroyOnLoad(this.gameObject);
+        source.Play();
+    }
+    public void OnFadeOut()
+    {
+        Destroy(this.gameObject);
+        PausedDueToBGM = false;
+        Loading.activeAnimator.SetFloat("speed", 1);
+        Active.GetComponent<Animator>().Play("BGMVolumeAni", 0, 0.0f);
+        Active.source.Play();
+    }
+}
