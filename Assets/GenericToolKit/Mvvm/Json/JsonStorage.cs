915d9bfebcba8e136436faee6b8b7d0a81e6ad11
OdericoOfficial <2919128969@qq.com>
2022-12-19 08:24:49 +0800

    成就简单逻辑
    早睡了喵，抱歉喵

--- Assets/GenericToolKit/Mvvm/Json/JsonStorage.cs
+++ Assets/GenericToolKit/Mvvm/Json/JsonStorage.cs
@@ -18,42 +18,42 @@ namespace GenericToolKit.Mvvm.Json
 
         public void Write<T>(string fileName, T obj)
         { 
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Create, FileAccess.Write))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Create, FileAccess.Write))
                using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
                 sw.WriteLine(JsonUtility.ToJson(obj));
         }
 
         public async ValueTask WriteAsync<T>(string fileName, T obj)
         {
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Create, FileAccess.Write))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Create, FileAccess.Write))
                 using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
                     await sw.WriteLineAsync(JsonUtility.ToJson(obj));
         }
 
         public void Append<T>(string fileName, T obj)
         {
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Append, FileAccess.Write))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Append, FileAccess.Write))
                 using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
                     sw.WriteLine(JsonUtility.ToJson(obj));
         }
 
         public async ValueTask AppendAsync<T>(string fileName, T obj)
         {
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Append, FileAccess.Write))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Append, FileAccess.Write))
                 using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
                     await sw.WriteLineAsync(JsonUtility.ToJson(obj));
         }
 
         public T Read<T>(string fileName)
         {
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Open, FileAccess.Read))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Open, FileAccess.Read))
                 using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
                     return JsonUtility.FromJson<T>(sr.ReadToEnd());
         }
 
         public async ValueTask<T> ReadAsync<T>(string fileName)
         {
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Open, FileAccess.Read))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Open, FileAccess.Read))
             using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
                 return JsonUtility.FromJson<T>(await sr.ReadToEndAsync());
         }
@@ -63,7 +63,7 @@ namespace GenericToolKit.Mvvm.Json
             IList<T> list = new List<T>();
             string[] jsons;
 
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Open, FileAccess.Read))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Open, FileAccess.Read))
                 using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
                     jsons = sr.ReadToEnd().Split("\r\n");
 
@@ -77,7 +77,7 @@ namespace GenericToolKit.Mvvm.Json
             IList<T> list = new List<T>();
             string[] jsons;
 
-            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.json"), FileMode.Open, FileAccess.Read))
+            using (FileStream fs = new FileStream(Path.Combine(GlobalConfiguration.JsonPath, $"{fileName}.txt"), FileMode.Open, FileAccess.Read))
             using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
                 jsons = (await sr.ReadToEndAsync()).Split('\r', '\n');
 
---
+++ Assets/Scripts/Achievement/AchievementCollection.cs
@@ -0,0 +1,52 @@
+using GenericToolKit.Mvvm;
+using GenericToolKit.Mvvm.Json;
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AchievementCollection : ObservableObject
+{
+    private readonly IList<AchievementModel> _achievementModels
+        = new List<AchievementModel>();
+    private readonly string _path = Path.Combine(GlobalConfiguration.JsonPath, "Achievements.txt");
+    private readonly IJsonStorage _jsonStorage;
+
+    public AchievementCollection(IJsonStorage jsonStorage)
+    {
+        _jsonStorage = jsonStorage;
+
+        if (!File.Exists(_path))
+        {
+            string jsonInfo = Resources.Load<TextAsset>("Achievements").text;
+            using (FileStream fs = new FileStream(_path, FileMode.Create, FileAccess.Write))
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                    sw.WriteLine(jsonInfo);
+        }
+
+        _achievementModels = _jsonStorage.ReadList<AchievementModel>("Achievements");
+    }
+
+    public AchievementModel this[AchievementEnum key]
+        => _achievementModels[(int)key];
+
+    public IEnumerable<AchievementModel> Achievements
+        => _achievementModels;
+
+    public bool IsAwarded(AchievementEnum key)
+        => Convert.ToBoolean(_achievementModels[(int)key].Awarded);
+
+    public void MakeAwarded(AchievementEnum key)
+        => _achievementModels[(int)key].Awarded = 1;
+
+    public void OnApplicationQuit()
+    {
+        _jsonStorage.Write("Achievements", _achievementModels[0]);
+        for (int i = 1; i < _achievementModels.Count; i++)
+            _jsonStorage.Append("Achievements", _achievementModels[i]);
+    }
+}
