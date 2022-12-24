1517e5853b2f79dd991ef1a6f0946fdcff827175
OdericoOfficial <2919128969@qq.com>
2022-12-24 16:22:29 +0800

    成就移动问题解决

--- Assets/Scripts/Achievement/AchievementTipVM.cs
+++ Assets/Scripts/Achievement/AchievementTipVM.cs
@@ -15,13 +15,16 @@ public class AchievementTipVM : ObservablePanel
 {
     public void ImageMovement(int index)
     {
+        if (ServiceLocator.Instance.AchievementCollection.IsAwarded((AchievementEnum)index))
+            return;
+
         Image image = GetCompoent<Image>($"Achievement{index}");
         Sequence sequence = DOTween.Sequence();
         sequence.AppendInterval(1f);
-        sequence.Append(image.rectTransform.DOLocalMoveX(-143, 4f).OnComplete(()
+        sequence.Append(image.rectTransform.DOAnchorPosX(-160, 4f).OnComplete(()
             =>
         {
-            image.rectTransform.DOLocalMoveX(340, 4f);
+            image.rectTransform.DOAnchorPosX(340, 4f);
         }));
 
         ServiceLocator.Instance.AchievementCollection.MakeAwarded((AchievementEnum)index);
